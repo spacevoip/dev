90d04be3fb78764f23450efb490265b37a6145d6
spacevoip <spacevoip1@gmail.com>
2025-01-06 10:48:00 -0300

    Adiciona configurações para suportar SPA routing e F5

--- vite.config.ts
+++ vite.config.ts
@@ -21,6 +21,7 @@ export default defineConfig(({ mode }) => {
           secure: false,
         },
       },
+      historyApiFallback: true, // Adiciona suporte ao history API fallback
     },
     resolve: {
       alias: {
